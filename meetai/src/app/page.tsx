bd688aec84db0655844ab90fc488ac2fabaddd0b
AniruddhTiwariiiiii <007aniruddhtiwari@gmail.com>
2025-06-02 01:52:23 +0530

    Setting things up

---
+++ meetai/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-5xl font-bold text-blue-300">
+      <h1>This is the Meet-Ai application</h1>
+      <Button variant="destructive">Click this button !</Button>
+    </div>
+  );
+}
