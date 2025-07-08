b295bfae69546fb6204e8abaca7afe7021f045db
AniruddhTiwariiiiii <007aniruddhtiwari@gmail.com>
2025-07-08 15:06:31 +0530

    16-Meetings Setup

---
+++ meetai/src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { LoadingState } from "@/components/ui/loading-state";
+import { ErrorState } from "@/components/ui/error-state";
+
+export const  MeetingsView = () => {
+    const trpc = useTRPC();
+    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+
+    return (
+        <div>
+            {JSON.stringify(data)}
+        </div>
+    );
+};
+
+export const MeetingsViewLoading = () => {
+    return (
+        <LoadingState 
+            title = "Loading Meetings"
+            description="This may take a few seconds"
+        />
+    )
+}
+
+export const MeetingsViewError = () => {
+    return (
+        <ErrorState 
+            title= "Error Loading Meetings"
+            description="Something went wrong while loading meetings. Please try again later."
+        />
+    )
+}
--- meetai/src/trpc/routers/_app.ts
+++ meetai/src/trpc/routers/_app.ts
@@ -2,10 +2,12 @@ import { agentsRouter } from '@/modules/agents/server/procedure';
 
 import {createTRPCRouter } from 
 '../init';
+import { meetingsRouter } from '@/modules/meetings/server/procedures';
 
 
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings: meetingsRouter,
 
 });
 // export type definition of API
