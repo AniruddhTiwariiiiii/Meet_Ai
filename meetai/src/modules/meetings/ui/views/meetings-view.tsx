dfe1f1bbdc983d73393a1dd2239851330612a5a6
Aniruddh-SceneON <aniruddhtiwari.work@gmail.com>
2025-07-17 02:15:11 +0530

    19-Meetings Filters

--- meetai/src/modules/meetings/ui/views/meetings-view.tsx
+++ meetai/src/modules/meetings/ui/views/meetings-view.tsx
@@ -7,14 +7,31 @@ import { ErrorState } from "@/components/ui/error-state";
 import { DataTable } from "@/components/ui/data-table";
 import { columns } from "../components/columns";
 import { EmptyState } from "@/components/ui/empty-state";
+import { useRouter } from "next/navigation";
+import { useMeetingsFilters } from "../../hooks/use-meetings-filter";
+import { DataPagination } from "@/components/ui/data-pagination";
+import { meetings } from "@/db/schema";
 
 export const  MeetingsView = () => {
     const trpc = useTRPC();
-    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+    const router = useRouter();
+    const [filters, setFilters] = useMeetingsFilters();
+
+
+    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({
+        ...filters,
+    }))
 
     return (
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable data={data.items} columns={columns}/>
+            <DataTable
+             data={data.items} columns={columns} onRowClick={(row) => router.push(`/meetings/${row.id}`
+             )}/>
+            <DataPagination 
+                page={filters.page}
+                totalPages={data.totalPages}
+                onPageChange={(page) => setFilters({ page })}
+            />
             {data.items.length === 0 && (
                 <EmptyState
                     title="Create your first meeting"
